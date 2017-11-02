bf88b3792cf9cf859cd1b1bb140e1e676efe7a5f
Sarakvitou <Sarakvitou@gmail.com>
2017-11-01 23:11:42 -0700

    Initial check-in of module FileIOCsharp

---
+++ FileIOCsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOCsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num = 5;
+            //Console.WriteLine("Enter a number: ");
+            //num = Console.Read();
+            for (int height = 0; height < num; height++)
+            {
+                int space = height;
+                int cntr = 0;
+                string line = "";
+                Console.Write(line.PadLeft(space, ' '));
+                while (cntr++ < (num - space))
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
